7fdada00fd3fce15abb7bffcfb3f9954ac13318a
a-2280 <khaliff.williamss@gmail.com>
2025-09-03 21:42:30 -0600

    need a quick commit

--- script.js
+++ script.js
@@ -1,36 +1,56 @@
 const forageButton = document.getElementById("forage");
 const sleepButton = document.getElementById("sleep");
+const caveButton = document.getElementById("cave");
 const food = document.getElementById("food");
-const day = document.getElementById("days")
+const day = document.getElementById("days");
+const monkeys = document.getElementById("monkeys");
+const understanding = document.getElementById("understanding");
 const messageBoard = document.getElementById("message-board");
 const messageContainer = document.querySelector(".message-container");
 
 let foodCount = 10;
 let monkeyCount = 3;
 let dayCount = 1;
-
-food.innerText = "Food:" + foodCount;
+let understandingCount = 0;
 
 const getRandomNumber = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 forageButton.addEventListener("click", () => {
   const randomNumber = getRandomNumber(0, 3);
-  console.log(randomNumber)
+  const understandingChance = Math.random();
 
-  foodCount += randomNumber;
-  food.innerText = "Food:" + foodCount;
+  if (understandingChance < 0.1) {
+    understandingCount += 1;
+    understanding.innerText = `Understanding: ${understandingCount}`;
 
-  const li = document.createElement("li");
-  li.appendChild(document.createTextNode(`You collected ${randomNumber} food`));
-  messageBoard.appendChild(li);
-  messageContainer.scrollTop = messageContainer.scrollHeight;
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(`Ape learn something!`));
+    messageBoard.appendChild(li);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+  } else {
+    foodCount += randomNumber;
+    food.innerText = `Food: ${foodCount}`;
+
+    const li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(`You collected ${randomNumber} food`)
+    );
+    messageBoard.appendChild(li);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+  }
 });
 
 sleepButton.addEventListener("click", () => {
+  if (foodCount == 0 || foodCount - monkeyCount < 0) {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(`Monkeys starve!`));
+    messageBoard.appendChild(li);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+  } else {
     foodCount -= monkeyCount;
     dayCount += 1;
 
@@ -38,7 +58,30 @@ sleepButton.addEventListener("click", () => {
     food.innerText = `Food: ${foodCount}`;
 
     const li = document.createElement("li");
-  li.appendChild(document.createTextNode(`Monkeys go to sleep`));
-  messageBoard.appendChild(li);
-  messageContainer.scrollTop = messageContainer.scrollHeight;
-})
+    li.appendChild(document.createTextNode(`Monkeys eat. Day passes.`));
+    messageBoard.appendChild(li);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+  }
+});
+
+caveButton.addEventListener("click", () => {
+  const randomNumber = Math.random();
+
+  if (randomNumber < 0.5) {
+    understandingCount += 3;
+    understanding.innerText = `Understanding: ${understandingCount}`;
+
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(`Understanding +3`));
+    messageBoard.appendChild(li);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+  } else {
+    monkeyCount -= 1;
+    monkeys.innerText = `Monkeys: ${monkeyCount}`;
+
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(`Monkey died...`));
+    messageBoard.appendChild(li);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+  }
+});
